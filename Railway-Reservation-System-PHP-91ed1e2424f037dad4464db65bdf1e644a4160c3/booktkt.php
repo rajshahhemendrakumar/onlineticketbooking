7d33b06edd8fbc877cc3377e895be94e9686766a
Raj Shah <rajshah468@gamil.com>
2024-01-05 13:34:00 +0530

    Initial commit

---
+++ Railway-Reservation-System-PHP-91ed1e2424f037dad4464db65bdf1e644a4160c3/booktkt.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Book a ticket</title>
+	<LINK REL="STYLESHEET" HREF="STYLE.CSS">
+	<style type="text/css">
+		#booktkt	{
+			margin:auto;
+			margin-top: 50px;
+			width: 40%;
+			height: 60%;
+			padding: auto;
+			padding-top: 50px;
+			padding-left: 50px;
+			background-color: rgba(0,0,0,0.3);
+			border-radius: 25px;
+		}
+		html { 
+		  background: url(img/bg7.jpg) no-repeat center center fixed; 
+		  -webkit-background-size: cover;
+		  -moz-background-size: cover;
+		  -o-background-size: cover;
+		  background-size: cover;
+		}
+		#journeytext	{
+			color: white;
+			font-size: 28px;
+			font-family:"Comic Sans MS", cursive, sans-serif;
+		}
+		#trains	{
+			margin-left: 90px;
+			font-size: 15px;
+		}
+		#submit	{
+			margin-left: 150px;
+			margin-bottom: 40px;
+			margin-top: 30px
+		}
+	</style>
+	<script type="text/javascript">
+		function validate()	{
+			var trains=document.getElementById("trains");
+			if(trains.selectedIndex==0)
+			{
+				alert("Please select your train");
+				trains.focus();
+				return false;		
+			}
+		}
+	</script>
+</head>
+<body>
+	
+	<div id="booktkt">
+	<h1 align="center" id="journeytext">Choose your journey</h1><br/><br/>
+	<form method="post" name="journeyform" onsubmit="return validate()">
+		<select id="trains" name="trains" required>
+			<option selected disabled>-------------------Select trains here----------------------</option>
+			<option value="rajdhani" >Rajdhani Express - Mumbai Central to Delhi</option>
+			<option value="duronto" >Duronto Express - Mumbai Central to Ernakulum</option>
+			<option value="geetanjali">Geetanjali Express - CST to Kolkata</option>
+			<option value="garibrath" >Garib Rath - Udaipur to Jammu Tawi</option>
+			<option value="mysoreexp" >Mysore Express - Talguppa to Mysore Jn</option>
+		</select>
+		<br/><br/>
+		<input type="submit" name="submit" id="submit" class="button" />
+	</form>
+	</div>
+	</body>
+	</html>
---
+++ bus-CU/bus-CU/Railway-Reservation-System-PHP-91ed1e2424f037dad4464db65bdf1e644a4160c3/pnrstatus.php
@@ -0,0 +1,39 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PNR Status</title>
+	<LINK REL="STYLESHEET" HREF="STYLE.CSS">
+	<style type="text/css">
+		#pnr	{
+			font-size: 20px;
+			background-color: white;
+			width: 500px;
+			height: 300px;
+			margin: auto;
+			border-radius: 25px;
+			border: 2px solid blue; 
+			margin: auto;
+  			position: absolute;
+  			left: 0; 
+  			right: 0;
+  			padding-top: 40px;
+  			padding-bottom:20px;
+  			margin-top: 130px;
+ 
+		}
+		html { 
+		  background: url(img/bg7.jpg) no-repeat center center fixed; 
+		  -webkit-background-size: cover;
+		  -moz-background-size: cover;
+		  -o-background-size: cover;
+		  background-size: cover;
+		}
+		#pnrtext	{
+			padding-top: 20px;
+		}
+	</style>
+</head>
+<body>
+</body>
+</html>
